b446448dc20f00f1c444dd0390ff7f968eae5d71
Alex Zhou <3150103078@zju.edu.cn>
2019-04-19 08:01:34 +0800

    Add test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+from blender.blender_utils import OpenGLRenderer
+import os
+from config import cfg
+import numpy as np
+from base_utils import PoseTransformer, read_pose, read_pickle, save_pickle
+class_type = 'cat'
+dir_path = os.path.join(cfg.LINEMOD_ORIG,'{}/data'.format(class_type))
+train_set = np.loadtxt(os.path.join(cfg.LINEMOD, '{}/training_range.txt'.format(class_type)),np.int32)
+for idx in train_set:
+        rot_path = os.path.join(dir_path, 'rot{}.rot'.format(idx))
+        tra_path = os.path.join(dir_path, 'tra{}.tra'.format(idx))
+        pose = read_pose(rot_path, tra_path) 
+        rot, tra = pose[:, :3], pose[:, 3]
+        break
+r = OpenGLRenderer()
+rgb = r.render(class_type, pose, intrinsic_matrix=r.intrinsic_matrix['linemod'], mode='rgb')
