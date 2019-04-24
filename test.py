9d29e8b6df46346aa43cd5f1351c1c6ece4bda5c
shzhou2 <alex_zhou@zju.edu.cn>
2019-04-24 15:14:26 +0800

    Fix error caused by pose transform

--- test.py
+++ test.py
@@ -4,16 +4,20 @@ from config import cfg
 import numpy as np
 from base_utils import PoseTransformer, read_pose, read_pickle, save_pickle
 from scipy.misc import imshow, imsave
+from base_utils import PoseTransformer
 class_type = 'cat'
 dir_path = os.path.join(cfg.LINEMOD_ORIG,'{}/data'.format(class_type))
 train_set = np.loadtxt(os.path.join(cfg.LINEMOD, '{}/training_range.txt'.format(class_type)),np.int32)
+
+trans = PoseTransformer(class_type)
+
 for idx in train_set:
         rot_path = os.path.join(dir_path, 'rot{}.rot'.format(idx))
         tra_path = os.path.join(dir_path, 'tra{}.tra'.format(idx))
         pose = read_pose(rot_path, tra_path) 
+        pose = trans.orig_pose_to_blender_pose(pose)
         rot, tra = pose[:, :3], pose[:, 3]
         break
 r = OpenGLRenderer()
 rgb = r.render(class_type, pose, intrinsic_matrix=r.intrinsic_matrix['linemod'], render_type='rgb')
 imshow(rgb) 
-#imsave(rgb, "RGB")
